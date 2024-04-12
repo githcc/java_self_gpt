9c50971a9f13c0fa39c0e5639d9a579beee81641
cc <wyswydx@gmail.com>
2024-04-12 11:11:39 +0800

    1. 调整格式，后续打算打包成jar

--- src/main/java/com/cc/controller/API.java
+++ src/main/java/com/cc/controller/API.java
@@ -29,7 +29,7 @@ public class API {
     public Flux<String> generateStream(@RequestBody String body) {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", OPENAI_API_KEY);
-        headers.set("content-type", "application/json");
+        headers.set("content-type", MediaType.APPLICATION_JSON_VALUE);
 
         return webClient.post()
                 .uri("/chat/completions")
@@ -44,12 +44,11 @@ public class API {
     public ResponseEntity<String> generateImage(@RequestBody String body) {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", OPENAI_API_KEY);
-        headers.set("content-type", "application/json");
-
+        headers.set("content-type", MediaType.APPLICATION_JSON_VALUE);
         HttpEntity<String> request = new HttpEntity<>(body, headers);
 
         return this.restTemplate.exchange(
-                OPENAI_API_URL+"/images/generations",
+                OPENAI_API_URL + "/images/generations",
                 HttpMethod.POST,
                 request,
                 String.class
@@ -58,23 +57,21 @@ public class API {
 
     @PostMapping(value = "/audio/transcriptions")
     public ResponseEntity<String> transcribeAudio(@RequestPart("file") MultipartFile file, @RequestParam("model") String model, @RequestParam("temperature") String temperature
-    , @RequestParam("response_format") String response_format, @RequestParam("language") String language) {
+            , @RequestParam("response_format") String response_format, @RequestParam("language") String language) {
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", OPENAI_API_KEY);
-        headers.set("content-type", "multipart/form-data");
+        headers.set("content-type", MediaType.MULTIPART_FORM_DATA_VALUE);
         MultiValueMap<String, Object> requestBody = new LinkedMultiValueMap<>();
         requestBody.add("file", file.getResource());
         requestBody.add("model", model);
         requestBody.add("temperature", temperature);
         requestBody.add("response_format", response_format);
         requestBody.add("language", language);
-
         HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(requestBody, headers);
 
-
         return this.restTemplate.exchange(
-                OPENAI_API_URL+"/audio/transcriptions",
+                OPENAI_API_URL + "/audio/transcriptions",
                 HttpMethod.POST,
                 request,
                 String.class
